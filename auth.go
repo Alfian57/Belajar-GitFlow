25e7c7c4d8b9cc585158d738d95c151540194683
Alfian Gading Saputra <109786221+Alfian57@users.noreply.github.com>
2025-02-11 05:24:14 +0700

    Features/auth (#1)
    * feat: login
    
    * feat: logout
    
    * feat: register
    
    * feat: forgot password

---
+++ auth.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func login() {
+	fmt.Println("Login")
+}
+
+func logout() {
+	fmt.Println("Logout")
+}
+
+func register() {
+	fmt.Println("Register")
+}
+
+func forgotPassword() {
+	fmt.Println("Forgot Password")
+}
