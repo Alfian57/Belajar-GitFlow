f098e50642d85c5c804c2c41b26a8807a11dcc44
Alfian Gading Saputra <109786221+Alfian57@users.noreply.github.com>
2025-02-11 08:11:06 +0700

    Dev (#5)
    * Features/auth (#1)
    
    * feat: login
    
    * feat: logout
    
    * feat: register
    
    * feat: forgot password
    
    * Features/category (#2)
    
    * feat: create category
    
    * feat: update category
    
    * feat: delete category
    
    * feat: get all category
    
    * Features/product (#4)
    
    * feat: create product
    
    * feat: update product

---
+++ product.go
@@ -0,0 +1,14 @@
+package main
+
+type Product struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func (p *Product) Create() {
+	// Create product
+}
+
+func (p *Product) Update() {
+	// Update product
+}
